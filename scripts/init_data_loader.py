ca3bcb1f1b2e88e1e922b4684077c864cb5ee0a6
S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>
2017-12-12 02:01:17 +0600

    Updated initial data load script.
    Signed-off-by: S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>

--- scripts/init_data_loader.py
+++ scripts/init_data_loader.py
@@ -7,5 +7,6 @@ admin_user_password = settings.DJANGO_ADMIN_SUPER_USER_PASSWORD
 
 if not User.objects.filter(email=admin_user_email).exists():
     User.objects.create_superuser(
+        username=admin_user_email,
         email=admin_user_email,
         password=admin_user_password)
